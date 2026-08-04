ae192dcb26a5011d4cbe807712a3eec7e0679dc8
Emmanuel FARHI <emmanuel.farhi@synchrotron-soleil.fr>
2026-08-04 15:45:27 +0200

    Create REQUIREMENTS.m

---
+++ NOTES/REQUIREMENTS.m
@@ -0,0 +1,12 @@
+# List of software to install in prepared environments for the school
+
+Packages to install (in alphabetical order):
+
+- cuda-toolkit
+- mcstas
+- mcxtrace
+- python3-joblib
+- python3-numpy
+- python3-scipy
+- python3-sklearn
+- python3-skimage
